47c137a48ff27a12f3a4e0568c50740bd6cf46ea
pgarbrecht <philgarbrecht@gmail.com>
2022-12-05 17:38:21 -0600

    home layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,32 @@
 import '../App.css';
+import { Link } from "react-router-dom";
 import ImageTile from './ImageTile'
 
 function Home(props) {
 
   return (
-    <div>
-      <p>Home</p>
-      <h2>{props.images.length} Images</h2>
-      {props.images.map((image, index) => {
-                        return (
-                            <ImageTile
-                            key={index}
-                            index={index}
-                            id = {image._id}
-                            title = {image.title}
-                            url = {image.url}
-                            />
-                        )
-                    }
-      )}
-      {/* <ImageTile /> */}
+    <div className='home-container'>
+      <div className='home-top-utilitybox'>
+        <p className='search'>Search</p>
+        <Link className='upload-button' to='/upload'>UPLOAD</Link>
+      </div>
+      <div className='home-grid-container'>
+        <h2 className='home-counter'>{props.images.length} Images</h2>
+        <div className='home-grid'>
+        {props.images.map((image, index) => {
+          return (
+            <ImageTile
+              key={index}
+              index={index}
+              id = {image._id}
+              title = {image.title}
+              url = {image.url}
+            />
+          )
+        }
+        )}
+        </div>
+      </div>
     </div>
   );
 }
